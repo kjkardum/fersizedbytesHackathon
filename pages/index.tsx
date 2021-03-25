9b8f81c7052bd79189a4804a7f748ebc15203902
kjkardum <kjkardum@gmail.com>
2021-03-25 12:42:38 +0100

    Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,29 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-import NavBar from "../components/NavBar"
-import HomeContent from "../components/HomeContent"
+import NavBar from "../components/NavBar";
+import HomeContent from "../components/HomeContent";
 
 export default function Home() {
     return (
-    <div className={styles.container}>
-      <Head>
-        <title>Takeoff</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NavBar></NavBar>
-      <main className={styles.main}>
-        <div className={styles.topblock}>
-          <div className={styles.title}>
-            Takeoff 🚀
-          </div>
-          <div className={styles.quote}>
-            Random quote iz pile-a
-          </div>
+        <div className={styles.container}>
+            <Head>
+                <title>Takeoff</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <NavBar></NavBar>
+            <main className={styles.main}>
+                <div className={styles.topblock}>
+                    <div className={styles.title}>Takeoff 🚀</div>
+                    <div className={styles.quote}>Random quote iz pile-a</div>
+                </div>
+                <div className={styles.contentblock}>
+                    <div className={styles.searchcontainer}>
+                        <input type="search" id="flightSearch" name="q" aria-label="Search flights" className={styles.searchbox} placeholder="Find a flight"></input>
+                    </div>
+                    <HomeContent></HomeContent>
+                </div>
+            </main>
         </div>
-        <div className={styles.contentblock}>
-          <div className={styles.searchcontainer}>
-            <input
-              type="search"
-              id="flightSearch"
-              name="q"
-              aria-label="Search flights"
-              className={styles.searchbox}
-              placeholder="Find a flight"></input>
-          </div>
-          <HomeContent></HomeContent>
-        </div>
-      </main>
-    </div>
     );
 }
