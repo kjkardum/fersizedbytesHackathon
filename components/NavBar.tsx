5f25e61886121c1d60b5be95b94ee88d5e0bfaae
nitko12 <nitko12@hotmail.com>
2021-03-25 12:33:14 +0100

    Navbar links fixed.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,12 +3,20 @@ import styles from "../styles/NavBar.module.css";
 
 const NavBar = (props) => (
     <div className={styles.navbar}>
-        <div className={styles['navbar-nav']}>
-          <a className={styles['navbar-item']} href="#">Home</a>
-          <a className={styles['navbar-item']} href="#">Buy Flight</a>
-          <a className={styles['navbar-item']} href="#">Make a reservation</a>
-          <a className={styles['navbar-item']} href="#">Login</a>
+        <div className={styles["navbar-nav"]}>
+            <a className={styles["navbar-item"]} href="/home">
+                Home
+            </a>
+            <a className={styles["navbar-item"]} href="/buy">
+                Buy Flight
+            </a>
+            <a className={styles["navbar-item"]} href="/reserve">
+                Make a reservation
+            </a>
+            <a className={styles["navbar-item"]} href="/login">
+                Login
+            </a>
         </div>
-      </div>
-)
+    </div>
+);
 export default NavBar;
