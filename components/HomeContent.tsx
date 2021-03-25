86148ffca9da18f0296b086423773583bbc6a78b
kjkardum <kjkardum@gmail.com>
2021-03-25 13:22:05 +0100

    Homepage UI

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -3,11 +3,13 @@ import styles from "../styles/HomeContent.module.css";
 
 import { Row, Col } from "react-bootstrap";
 import HomeFeatured from "./HomeFeatured";
+import FlightMap from "./FlightMap";
 
 const HomeContent = (props) => (
     <div className={styles.homecontent}>
         <HomeFeatured></HomeFeatured>
         <HomeFeatured reverse={true}></HomeFeatured>
+        <FlightMap></FlightMap>
     </div>
 );
 export default HomeContent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 
 import NavBar from "../components/NavBar";
 import HomeContent from "../components/HomeContent";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
@@ -22,6 +23,7 @@ export default function Home() {
                         <input type="search" id="flightSearch" name="q" aria-label="Search flights" className={styles.searchbox} placeholder="Find a flight"></input>
                     </div>
                     <HomeContent></HomeContent>
+                    <Footer></Footer>
                 </div>
             </main>
         </div>
