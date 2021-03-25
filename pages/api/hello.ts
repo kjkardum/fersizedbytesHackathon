01d10b2ca4b84d3cd5c983aba093872e0a49bdfb
nitko12 <nitko12@hotmail.com>
2021-03-25 10:57:04 +0100

    Fixed merge collision.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,9 +3,5 @@
 import { APIWrapper } from "../../services/ApiWrapper";
 
 export default (req, res) => {
-<<<<<<< HEAD:pages/api/hello.ts
-	let test = new APIWrapper();
-=======
-    res.status(200).json({ name: "John Doe" });
->>>>>>> 5221226356e828624563b8ca0b0d9c32716216fc:pages/api/hello.js
+    let test = new APIWrapper();
 };
