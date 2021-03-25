94d465a54583bee204d5354fa3e50279d889a614
kjkardum <kjkardum@gmail.com>
2021-03-25 15:24:37 +0100

    NavBar edit

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,37 +3,36 @@ import styles from "../styles/NavBar.module.css";
 import { faEllipsisV, fas, faSearch, faHome, faPlane, faAddressCard, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-<<<<<<< HEAD
 const NavBar = (props) => {
     let [navbox, toggleNavbox] = useState(false);
     return (
         <div className={styles.navbar}>
             <div className={styles["navbar-nav"]}>
-                <a className={styles["navbar-item"]} href="#">
+                <a className={styles["navbar-item"]} href="/">
                     Home
                 </a>
-                <a className={styles["navbar-item"]} href="#">
+                <a className={styles["navbar-item"]} href="/buy">
                     Buy Flight
                 </a>
-                <a className={styles["navbar-item"]} href="#">
+                <a className={styles["navbar-item"]} href="/reserve">
                     Make a reservation
                 </a>
-                <a className={styles["navbar-item"]} href="#">
+                <a className={styles["navbar-item"]} href="/login">
                     Login
                 </a>
             </div>
             <div className={styles.navbuttons}>
                 <div className={`${styles.navbox} bigshadow` + (navbox ? ` ${styles.navboxactive}` : "")}>
-                    <a className={styles.navboxlink} href="#" onClick={() => toggleNavbox(false)}>
+                    <a className={styles.navboxlink} href="/" onClick={() => toggleNavbox(false)}>
                         <FontAwesomeIcon icon={faHome} className={styles.navboxicon}></FontAwesomeIcon> Home
                     </a>
-                    <a className={styles.navboxlink} href="#" onClick={() => toggleNavbox(false)}>
+                    <a className={styles.navboxlink} href="/buy" onClick={() => toggleNavbox(false)}>
                         <FontAwesomeIcon icon={faPlane} className={styles.navboxicon}></FontAwesomeIcon> Buy tickets
                     </a>
-                    <a className={styles.navboxlink} href="#" onClick={() => toggleNavbox(false)}>
+                    <a className={styles.navboxlink} href="/reserve" onClick={() => toggleNavbox(false)}>
                         <FontAwesomeIcon icon={faAddressCard} className={styles.navboxicon}></FontAwesomeIcon> Reservations
                     </a>
-                    <a className={styles.navboxlink} href="#" onClick={() => toggleNavbox(false)}>
+                    <a className={styles.navboxlink} href="/login" onClick={() => toggleNavbox(false)}>
                         <FontAwesomeIcon icon={faUser} className={styles.navboxicon}></FontAwesomeIcon> Login
                     </a>
                 </div>
@@ -44,23 +43,6 @@ const NavBar = (props) => {
                     <FontAwesomeIcon icon={faEllipsisV} className={styles.navicon} onClick={() => toggleNavbox(!navbox)} />
                 </div>
             </div>
-=======
-const NavBar = (props) => (
-    <div className={styles.navbar}>
-        <div className={styles["navbar-nav"]}>
-            <a className={styles["navbar-item"]} href="/home">
-                Home
-            </a>
-            <a className={styles["navbar-item"]} href="/buy">
-                Buy Flight
-            </a>
-            <a className={styles["navbar-item"]} href="/reserve">
-                Make a reservation
-            </a>
-            <a className={styles["navbar-item"]} href="/login">
-                Login
-            </a>
->>>>>>> b1d4dbd695ea3452c33648b5696cb22248203a85
         </div>
     );
 };
