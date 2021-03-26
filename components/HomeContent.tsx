3111176b9ee1a141b1bdc7a726ecdaf562f2ffe1
kjkardum <kjkardum@gmail.com>
2021-03-26 06:38:35 +0100

    Homepage featured

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -8,9 +8,9 @@ import { FlightMap } from "./FlightMap";
 const HomeContent = (props) => (
     <div className={styles.homecontent}>
         <div className={styles.featureddestinations}>Popular Destination</div>
-        <HomeFeatured></HomeFeatured>
+        <HomeFeatured description="Rio de Janeiro is the second largest city in Brazil, on the South Atlantic coast. Rio is famous for its breathtaking landscape, its laid back beach culture and its annual carnival. Although, their soccer skills here are very well recognized." title="Rio de Janeiro" image="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"></HomeFeatured>
         <hr className={styles.mobilehr}></hr>
-        <HomeFeatured reverse={true}></HomeFeatured>
+        <HomeFeatured reverse={true} title="London" description="London, city, capital of the United Kingdom. It is among the oldest of the world's great cities—its history spanning nearly two millennia—and one of the most cosmopolitan. By far Britain's largest metropolis, it is also the country's economic, transportation, and cultural centre." image="https://images.adsttc.com/media/images/5ced/70a5/284d/d1e7/0300/041f/large_jpg/03_cutaway-london-big-ben.jpg?1559064722"></HomeFeatured>
         <hr className={styles.mobilehr}></hr>
 
         <FlightMap
--- components/HomeFeatured.tsx
+++ components/HomeFeatured.tsx
@@ -10,11 +10,11 @@ const HomeFeatured = (props) => {
     return (
         <Row className={styles.featured}>
             <Col className={styles.featuredimagebox} md={{ order: !props.reverse ? "first" : "last" }}>
-                <img src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" className={`${styles.featuredimage} bigshadow`}></img>
+                <img src={props.image} className={`${styles.featuredimage} bigshadow`}></img>
             </Col>
             <Col className={styles.featureddetails} md={{ order: !props.reverse ? "last" : "first" }}>
                 <div className={styles.featuredtitle}>
-                    Rio de Janeiro
+                    {props.title}
                     <FontAwesomeIcon icon={faCaretDown} className={styles.featuredexpand} onClick={() => open(!isOpen)}></FontAwesomeIcon>
                 </div>
                 <div className={styles.featuredicons}>
@@ -25,9 +25,9 @@ const HomeFeatured = (props) => {
                         <FontAwesomeIcon icon={faUmbrellaBeach} className={styles.featuredicon} />
                     </div>
                 </div>
-                <div className={styles.featureddescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac sem rutrum, pulvinar ipsum eu, sollicitudin magna. Maecenas auctor venenatis dolor, porttitor dictum augue eleifend a. Etiam mollis cursus tortor a condimentum. Fusce est ipsum, luctus id elit eget, fermentum tempor ligula. Nulla in sem ultricies ex congue pulvinar. Duis finibus nulla sed ornare interdum. Curabitur vulputate gravida sem, sed viverra elit lacinia nec. Nunc facilisis justo et quam tempus, ornare bibendum nisl suscipit. Mauris et mattis justo. Suspendisse potenti. Curabitur iaculis augue ante, sit amet viverra enim ultrices ac.</div>
+                <div className={styles.featureddescription}>{props.description}</div>
             </Col>
-            <div className={`${styles.mobiledescription}` + (isOpen ? ` ${styles.mobiledescriptionactive}` : ``)}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac sem rutrum, pulvinar ipsum eu, sollicitudin magna. Maecenas auctor venenatis dolor, porttitor dictum augue eleifend a. Etiam mollis cursus tortor a condimentum. Fusce est ipsum, luctus id elit eget, fermentum tempor ligula. Nulla in sem ultricies ex congue pulvinar. Duis finibus nulla sed ornare interdum. Curabitur vulputate gravida sem, sed viverra elit lacinia nec. Nunc facilisis justo et quam tempus, ornare bibendum nisl suscipit. Mauris et mattis justo. Suspendisse potenti. Curabitur iaculis augue ante, sit amet viverra enim ultrices ac.</div>
+            <div className={`${styles.mobiledescription}` + (isOpen ? ` ${styles.mobiledescriptionactive}` : ``)}>{props.description}</div>
         </Row>
     );
 };
